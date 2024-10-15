6f8d8061a559eaf6e2c9aead3f2532bb82185778
Anjali20yadav <anjali.kmri2541@gmail.com>
2024-10-15 17:05:05 +0530

    Add files via upload

---
+++ Randomcolor generator/app.js
@@ -0,0 +1,17 @@
+let btn=document.querySelector("button");
+function randomcolorgenerator(){
+    let red=Math.floor(Math.random()*255);
+    let green=Math.floor(Math.random()*255);
+    let blue=Math.floor(Math.random()*255);
+    let color=`rgb(${red},${green},${blue})`;
+    return color;
+}
+btn.addEventListener("click",function(){
+    let h3=document.querySelector("h3");
+    let randomcolor= randomcolorgenerator();
+    h3.innerHTML=randomcolor;
+    let div=document.querySelector("div");
+   
+    div.style.backgroundColor=randomcolor;
+});
+
